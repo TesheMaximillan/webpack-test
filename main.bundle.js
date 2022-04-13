ede1cf43002ff7fe15ad5d9037d53afdc611bac5
buhariaminat <buhariaminat@yahoo.com>
2022-04-13 22:23:43 +0100

    Add popup dynamically

--- main.bundle.js
+++ main.bundle.js
@@ -16,7 +16,7 @@
   \****************************************************************************************************/
 /***/ ((module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/noSourceMaps.js */ \"./node_modules/css-loader/dist/runtime/noSourceMaps.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);\n// Imports\n\n\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));\n___CSS_LOADER_EXPORT___.push([module.id, \"@import url(https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&family=Roboto:ital,wght@0,400;0,700;1,500&display=swap);\"]);\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, \"* {\\n  margin: 0;\\n  padding: 0; }\\n\\n*,\\n*::before,\\n*::after {\\n  box-sizing: inherit; }\\n\\nhtml {\\n  box-sizing: border-box;\\n  font-size: 62.5%; }\\n\\nbody {\\n  font-family: \\\"Montserrat\\\", sans-serif;\\n  font-weight: 400;\\n  line-height: 1.6;\\n  font-size: 1.6rem; }\\n\\n.container {\\n  color: red; }\\n\\n.body {\\n  color: red; }\\n\", \"\"]);\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack://webpack-setup/./src/main.scss?./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/noSourceMaps.js */ \"./node_modules/css-loader/dist/runtime/noSourceMaps.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);\n// Imports\n\n\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));\n___CSS_LOADER_EXPORT___.push([module.id, \"@import url(https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&family=Roboto:ital,wght@0,400;0,700;1,500&display=swap);\"]);\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, \"* {\\n  margin: 0;\\n  padding: 0; }\\n\\n*,\\n*::before,\\n*::after {\\n  box-sizing: inherit; }\\n\\nhtml {\\n  box-sizing: border-box;\\n  font-size: 62.5%; }\\n\\nbody {\\n  font-family: \\\"Montserrat\\\", sans-serif;\\n  font-weight: 400;\\n  line-height: 1.6;\\n  font-size: 1.6rem; }\\n\\n.container {\\n  color: red; }\\n\\n.pop-img {\\n  width: 60%;\\n  margin-top: 10px; }\\n\\n.comment-popup {\\n  margin: 20px;\\n  border: 3px solid black; }\\n\\nh2 {\\n  text-align: center;\\n  margin-top: 10px; }\\n\\n.about {\\n  display: flex;\\n  flex-wrap: wrap;\\n  justify-content: space-around; }\\n\\n.description {\\n  display: flex;\\n  flex-direction: column; }\\n\\n.des {\\n  display: flex;\\n  flex-direction: column; }\\n\\n.container-img {\\n  display: flex;\\n  flex-flow: row-reverse;\\n  justify-content: center;\\n  padding-left: 120px;\\n  gap: 100px; }\\n\\nh5 {\\n  text-align: center;\\n  padding-top: 20px; }\\n\\np {\\n  font-size: 1.5rem;\\n  margin: 0%;\\n  padding: 0%; }\\n\\n.date {\\n  padding-left: 10%;\\n  font-size: 1.2rem;\\n  padding-top: 5px; }\\n\\nform {\\n  display: flex;\\n  flex-direction: column;\\n  gap: 20px;\\n  width: 50%;\\n  padding-left: 10%; }\\n\\n#name {\\n  width: 60%;\\n  border: 2px solid black;\\n  margin-top: 15px; }\\n\\ntextarea {\\n  border: 2px solid black; }\\n\\nbutton {\\n  width: 70%;\\n  border: 2px solid black;\\n  background-color: transparent;\\n  box-shadow: 2px 3px;\\n  margin-bottom: 20px; }\\n\\ni {\\n  color: black; }\\n\", \"\"]);\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack://webpack-setup/./src/main.scss?./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js");
 
 /***/ }),
 
@@ -116,7 +116,27 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./main.scss */ \"./src/main.scss\");\n/* eslint-disable import/extensions */\n\n\n\n//# sourceURL=webpack://webpack-setup/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./main.scss */ \"./src/main.scss\");\n/* harmony import */ var _modules_picture__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/picture */ \"./src/modules/picture.js\");\n/* eslint-disable import/extensions */\n\n\n\n(0,_modules_picture__WEBPACK_IMPORTED_MODULE_1__[\"default\"])();\n\n\n//# sourceURL=webpack://webpack-setup/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/modules/picture.js":
+/*!********************************!*\
+  !*** ./src/modules/picture.js ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ displayPopup)\n/* harmony export */ });\n/* harmony import */ var _assets_images_pancake_jpg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../assets/images/pancake.jpg */ \"./src/assets/images/pancake.jpg\");\n\n\nconst popContainer = document.querySelector('.comment-popup');\n\n// /////////////////// Create dom elements\n\nconst createElement = (\n  element = '',\n  className = [],\n  text = '',\n  src = '',\n  alt = '',\n  href = '',\n  type = '',\n  name = '',\n  id = '',\n  placeholder = '',\n  cols = '',\n  rows = '',\n) => {\n  const elements = document.createElement(element);\n  elements.classList.add(...className);\n  elements.textContent = text;\n  elements.src = src;\n  elements.alt = alt;\n  elements.href = href;\n  elements.type = type;\n  elements.name = name;\n  elements.id = id;\n  elements.placeholder = placeholder;\n  elements.cols = cols;\n  elements.rows = rows;\n\n  return elements;\n};\n\nconst displayPopup = () => {\n  const containerImage = createElement('div', ['container-img']);\n  const img = createElement('img', ['pop-img'], '', _assets_images_pancake_jpg__WEBPACK_IMPORTED_MODULE_0__, 'pancake');\n  const anchor = createElement('a', '', '', '', '', '#');\n  const icons = createElement('i', ['fa-solid', 'fa-xmark', 'fa-3x']);\n  const firstHeader = createElement('h2', '', 'Menu 3');\n  const aboutContainer = createElement(' div', ['about']);\n  const descriptionCont = createElement('div', ['description']);\n  const paragraphOne = createElement('p', '', 'Food: pancake');\n  const paragraphTwo = createElement('p', '', 'Origin: America');\n  const desCont = createElement('div', ['des']);\n  const paragraphThree = createElement('p', '', 'Calories: 200kcal');\n  const paragraphFour = createElement('p', '', 'Toppings: Chocolate Syrup');\n  const secondHeader = createElement('h5', '', 'Comments(2)');\n  const paragraphFive = createElement(\n    'p',\n    'date',\n    '03/04/2022 Priya: The perfect morning breakfast!',\n  );\n  const paragraphSix = createElement('p', 'date', '09/04/2022 Alex: I love');\n  const thirdHeader = createElement('h5', '', 'Add a comment');\n  const form = createElement('form');\n  const input = createElement(\n    'input',\n    '',\n    '',\n    '',\n    '',\n    '',\n    'text',\n    'name',\n    'name',\n    'Your name',\n  );\n  const textarea = createElement(\n    'textarea',\n    '',\n    '',\n    '',\n    '',\n    '',\n    '',\n    'textarea',\n    'textarea',\n    'Your insights',\n    '30',\n    '5',\n  );\n  const button = createElement('button', '', 'comment');\n\n  containerImage.appendChild(img, anchor);\n  anchor.appendChild(icons);\n  descriptionCont.appendChild(paragraphOne, paragraphTwo);\n  desCont.appendChild(paragraphThree, paragraphFour);\n  aboutContainer.appendChild(descriptionCont, desCont);\n  form.appendChild(input, textarea, button);\n\n  popContainer.append(paragraphOne);\n};\n\n\n\n\n//# sourceURL=webpack://webpack-setup/./src/modules/picture.js?");
+
+/***/ }),
+
+/***/ "./src/assets/images/pancake.jpg":
+/*!***************************************!*\
+  !*** ./src/assets/images/pancake.jpg ***!
+  \***************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"a9ce9bf342e38efcc08c.jpg\";\n\n//# sourceURL=webpack://webpack-setup/./src/assets/images/pancake.jpg?");
 
 /***/ })
 
@@ -171,6 +191,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _mai
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
 /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
@@ -185,6 +217,26 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _mai
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
 /******/ 	})();
 /******/ 	
 /************************************************************************/
